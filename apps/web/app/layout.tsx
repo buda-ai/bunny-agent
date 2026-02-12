551455fde5e04e0927b47ab2db2aea330ffa9cb9
Kelly Chan <kellypeilinchan@gmail.com>
2026-02-12 09:49:45 +0800

    Add Google Analytics using @next/third-parties

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function Layout({ children }: LayoutProps<"/">) {
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
+      <GoogleAnalytics gaId="G-B1FLZ40NXT" />
     </html>
   );
 }
